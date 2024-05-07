d4608b265c630ea34dc6f24ca7597333adbc227c
3zzazakl <vanzekovichgr@gmail.com>
2024-05-07 06:10:30 +0300

    check put

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -54,13 +54,15 @@ def post_state():
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def put_state(state_id):
     """ Updates a State object """
-    state = storage.get("State", state_id)
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    if not request.json:
+    if request.is_json:
+        data = request.get_json()
+        for key, value in data.items():
+            if key not in ['id', 'created_at', 'updated_at']:
+                setattr(state, key, value)
+        state.save()
+        return jsonify(state.to_dict()), 200
+    else:
         abort(400, 'Not a JSON')
-    for key, value in request.json.items():
-        if key not in ['id', 'created_at', 'updated_at']:
-            setattr(state, key, value)
-    state.save()
-    return jsonify(state.to_dict()), 200
